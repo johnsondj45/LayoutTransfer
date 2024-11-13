c413c15f748afcf1f9c2f34dce4af92d7458b20c
Dylan Johnson <dj2330@gmail.com>
2024-11-12 22:44:54 -0500

    Set up new directory and main layout file

--- app/src/main/java/com/example/layouttransfer/MainActivity.kt
+++ app/src/main/java/com/example/layouttransfer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.layouttransfer
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,35 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.layouttransfer.ui.theme.LayoutTransferTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            LayoutTransferTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+class MainActivity : Activity(){
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LayoutTransferTheme {
-        Greeting("Android")
-    }
 }
